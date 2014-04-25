aab9283dd69ee08b4b427dfdf2b8ed221e243578
Claus <Claus@192.168.199.1>
2014-04-25 13:55:25 +0200

    Jacobs mor elsker PIND-IS

---
+++ Git Hub test123/src/git/hub/test123/GitHubTest123.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package git.hub.test123;
+
+/**
+ *
+ * @author Claus
+ */
+public class GitHubTest123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("SÅ ER DER PIND-IS!");
+    }
+    
+}
